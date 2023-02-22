ee8738ca3140e18815dad24700fbe3d1ca20da5a
majo <majotorresacevedo@gmail.com>
2023-02-22 18:57:32 -0500

    Se cambia codigo, creando elementos en js, añadiendole clases y codigo del icono checkComplete

--- script.js
+++ script.js
@@ -5,19 +5,37 @@ const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
-  const list = document.querySelector("[data-list]")
-  const task = document.createElement("li")
-  task.classList.add("card")
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card");
   input.value = "";
+
+  const taskContent = document.createElement("div");
+  taskContent.appendChild(checkComplete());
+  const titleTask = document.createElement("span")
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(titleTask);
   const content = `
-  <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
+
   <i class="fas fa-trash-alt trashIcon icon"></i>
-`
-task.innerHTML = content;
-list.appendChild(task)
+    `;
+  //task.innerHTML = content;
+  task.appendChild(taskContent);
+
+  list.appendChild(task);
+
   console.log(content);
 };
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+  const i = document.createElement("i");
+  i.classList.add("far","fa-check-square","icon");
+  i.addEventListener("click", completeTask)
+  return i;
+};
+
+const completeTask =() =>{
+    console.log("complete task")
+}
