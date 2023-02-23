6f37842f948cbbe0c1397af135acd7801fb1b6d2
majo <majotorresacevedo@gmail.com>
2023-02-22 22:48:45 -0500

    Se modulariza el JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-(()=> {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
@@ -25,29 +27,5 @@ const createTask = (evento) => {
 
 btn.addEventListener("click", createTask);
 
-const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far","fa-check-square","icon");
-  i.addEventListener("click", completeTask)
-  return i;
-};
 
-const completeTask =(event) =>{
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-}
 
-const deleteIcon = () =>{
-    const i = document.createElement("i")
-    i.classList.add("fas","fa-trash-alt", "trashIcon", "icon")
-    i.addEventListener("click", deleteTask)
-    return i;
-}
-
-const deleteTask = (evento) => {
-    const parent = evento.target.parentElement;
-    parent.remove();
-}
-})()
